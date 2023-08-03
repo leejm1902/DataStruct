7a397ab0f2c3038761a3274d11971484c520f908
leejm1902 <leejm1902@gmail.com>
2023-08-03 20:04:31 +0900

    (2023.8.1) 그래프

--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,281 +1,90 @@
 ﻿#include <iostream>
+#include <vector>
 
-#define SIZE 10
+#pragma region 그래프
 
-int quickArray[SIZE] = { 4,1,2,3,9,7,8,6,10,5 };
-
-void QuickSort(int data[], int start, int end)
-{
-    // 원소가 1개만 있을 경우
-    if (start >= end)
-    {
-        return;
-    }
-
-    int pivot = start;
-    int left = start + 1;
-    int right = end;
-
-    while (left <= right)
-    {
-        // left가 end보다 작거나 같다면 반복
-        // 왼쪽에 있는 left 변수를 pivot과 비교합니다.
-        while (left <= end && quickArray[left] < quickArray[pivot])
-        {
-            left++;
-        }
-
-        // right가 start보다 크다면
-        // 오른쪽에 있는 right 변수를 pivot과 비교합니다.
-        while (right > start && quickArray[right] > quickArray[pivot])
-        {
-            right--;
-        }
-
-        // if 왼쪽과 오른쪽 엇갈렸을 때
-        if (left > right)
-        {
-            std::swap(quickArray[pivot], quickArray[right]);
-        }
-
-        else // else right와 left 값을 교환합니다.
-        {
-            std::swap(quickArray[left], quickArray[right]);
-        }
-
-        QuickSort(quickArray, start, right - 1);
-        QuickSort(quickArray, right + 1, end);
-    }
-}
-
-struct Node
-{
-    int key;
-    int value;
-
-    Node* next;
-};
-
-struct Bucket
-{
-    Node* head;
-    int count;
-};
-
-class HashTable
-{
-private:
-    Bucket bucket [5];
-
-public:
-    HashTable()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            bucket[i].count = 0;
-            bucket[i].head = nullptr;
-        }
-    }
-
-    Node* CreateNode(int key, int value)
-    {
-        // 1. 새로운 노드를 생성합니다.
-        Node* newNode = new Node;
-
-        // 2. 새로운 노드의 key값을 저장합니다.
-        newNode->key = key;
-
-        // 3. 새로운 노드의 value값을 저장합니다.
-        newNode->value = value;
-
-        // 4. 새로운 노드의 next값을 초기화합니다.
-        newNode->next = nullptr;
-        
-        // 5. 새로운 노드의 주소값을 반환합니다.
-        return newNode;
-    }
-
-    int HashFunction(int key)
-    {
-        return key % 5;
-    }
-
-    void Insert(int key, int value)
-    {
-        // 해시함수를 통해서 값을 받는 임시 변수
-        int hashIndex = HashFunction(key);
-
-        // 새로운 노드를 생성
-        Node* newNode = CreateNode(key, value);
-
-        // 노드가 1개라도 존재하지 않는다면
-        if (bucket[hashIndex].count == 0)
-        {
-            // 1. bucket[hashIndex]의 head 포인터에 새로운 노드의 주소값을 저장합니다.
-            bucket[hashIndex].head = newNode;
-
-            // 2. bucket[hashIndex]의 count 변수의 값을 증가합니다.
-            bucket[hashIndex].count++;
-        }
-
-        // 노드가 1개라도 존재하면
-        else
-        {
-            // 1. newNode의 next에 bucket[hashIndex]의 head값을 넣어줍니다.
-            newNode->next = bucket[hashIndex].head;
-
-            // 2. bucket[hashIndex].head를 방금 새로 생성한 node의 주소를 가리키도록 설정합니다.
-            bucket[hashIndex].head = newNode;
-
-            // 3. bucket[hashIndex]의 count 변수의 값을 증가합니다.
-            bucket[hashIndex].count++;
-        }
-    }
-
-    void Remove(int key)
-    {
-        // 해시함수를 통해서 값을 받는 임시 변수
-        int hashIndex = HashFunction(key);
-
-        // Node를 탐색할 수 있는 순회용 포인터 변수 선언 <- 각 bucket의 head값을 저장합니다.
-        Node* currentNode = bucket[hashIndex].head;
-
-        Node* traceNode = nullptr;
-
-        // currentNode가 nullptr이라면 함수를 종료합니다.
-        if (currentNode == nullptr)
-        {
-            std::cout << "노드가 존재하지 않습니다." << std::endl;
-            return;
-        }
-
-        // currentNode를 이용해서 내가 찾고자 하는 key값을 찾으면 됩니다.
-        while (currentNode != nullptr)
-        {
-            if (currentNode->key == key)
-            {
-                // 내가 삭제하고자 하는 key가 head에 있는 노드라면
-                if (currentNode == bucket[hashIndex].head)
-                {
-                    bucket[hashIndex].head = currentNode->next;
-                }
-
-                else
-                {
-                    traceNode->next = currentNode->next;
-                }
-
-                // 각 bucket의 count값을 감소시킵니다.
-                bucket[hashIndex].count--;
-
-                // 해당하는 메모리를 해제합니다.
-                delete currentNode;
-
-                return;
-            }
-
-            traceNode = currentNode;
-            currentNode = currentNode->next;
-        }
-
-    }
-
-    void Search(int key)
-    {
-        // 해시함수를 통해서 값을 받는 임시 변수
-        int hashIndex = HashFunction(key);
-
-        // Node를 탐색할 수 있는 순회용 포인터 변수 선언 <- 각 bucket의 head값을 저장합니다.
-        Node* currentNode = bucket[hashIndex].head;
-
-        // currentNode가 nullptr이라면 함수를 종료합니다.
-        if (currentNode == nullptr)
-        {
-            std::cout << "노드가 존재하지 않습니다." << std::endl;
-            return;
-        }
-
-        // currentNode를 이용해서 내가 찾고자 하는 key값을 찾으면 됩니다.
-        while (currentNode != nullptr)
-        {
-            // 해당하는 key값이 있다면 함수를 종료하고 해당하는 key값과 value값을 출력합니다.
-            if (currentNode->key == key)
-            {
-                std::cout << "Key : " << currentNode->key << "Value : " << currentNode->value << std::endl;
-                return;
-            }
-
-            // 해당하는 key값이 없다면 다음 Node의 주소를 저장합니다.
-            currentNode = currentNode->next;
-        }
-
-        std::cout << "찾고자 하는 키가 존재하지 않습니다." << std::endl;
-    }
-
-    void Show()
-    {
-
-    }
-
-};
-
-
+#pragma endregion
 
 
 int main()
 {
 
-#pragma region 퀵 정렬
-    // // 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고
-    // // 한 쪽에는 기준점보다 작은 항목들이 위치하고 다른 쪽에는 기준점보다 큰 항목들이 위치한다.
-    // QuickSort(quickArray, 0, SIZE - 1);
+#pragma region 인접 행렬
+    // // 그래프의 연결 관계를 이차원 배열로 나타내는 방식입니다.
     // 
-    // for (int i = 0; i < SIZE; i++)
+    // int buffer[4][4] = { 0, };
+    // 
+    // int vertex = 0;
+    // int edge = 0;
+    // 
+    // int x, y = 0;
+    // 
+    // // 1. vertex와 edge 값을 입력합니다.
+    // std::cout << "vertex를 입력해주세요." << std::endl;
+    // std::cin >> vertex;
+    // 
+    // std::cout << "edge를 입력해주세요." << std::endl;
+    // std::cin >> edge;
+    // 
+    // // 2. edge만큼 반복해서 x와 y값을 입력할 수 있도록 설정합니다.
+    // for (int i = 0; i < edge; i++)
     // {
-    //     std::cout << quickArray[i] << " ";
+    //     std::cout << "x의 값을 입력해주세요." << std::endl;
+    //     std::cin >> x;
+    // 
+    //     std::cout << "y의 값을 입력해주세요." << std::endl;
+    //     std::cin >> y;
+    // 
+    //     std::cout << "------------------------------------" << std::endl;
+    // 
+    //     buffer[x][y] = 1;
+    //     buffer[y][x] = 1;
     // }
     // 
+    // std::cout << "------------------------------------" << std::endl;
     // 
-    // // 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
-    // // 모든 배열이 기본 배열(요소가 하나뿐인 배열)이 될 때까지 반복하는 알고리즘입니다.
-
-
-
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         std::cout << buffer[i][j];
+    //     }
+    // 
+    //     std::cout << std::endl;
+    // }
 #pragma endregion
 
-#pragma region 해시 테이블
-    // (Key, Value)로 데이터를 저장하는 자료구조 중 하나로
-    // 빠르게 데이터를 검색할 수 있는 자료구조입니다.
+#pragma region 인접 리스트
+    // 그래프의 연결 관계를 vector의 배열(vector<int> data[])로 나타내는 배열입니다.
 
-    // 해시 테이블의 평균 시간 복잡도는 O(1)입니다.
+    int node = 0;
+    int edge = 0;
 
-    // 체이닝 기법
-    // 각 해시 버킷을 연결리스트로 구성하는 방식입니다.
-    HashTable hashTable;
+    int x, y = 0;
 
-    hashTable.Insert(5, 555);
-    hashTable.Insert(10, 111);
+    // 1. vertex와 edge 값을 입력합니다.
+    std::cout << "node를 입력해주세요." << std::endl;
+    std::cin >> node;
+    
+    std::cout << "edge를 입력해주세요." << std::endl;
+    std::cin >> edge;
 
-    hashTable.Search(7);
+    std::vector<int> data[4];
 
-    // 해시 충돌 발생 시 동일한 해시 값에 해당하는 데이터들을 연결리스트로 연결하여 저장합니다.
+    for (int i = 0; i < edge; i++)
+    {
+        std::cout << "x의 값을 입력해주세요." << std::endl;
+        std::cin >> x;
+        
+        std::cout << "y의 값을 입력해주세요." << std::endl;
+        std::cin >> y;
+        
+        std::cout << "------------------------------------" << std::endl;
 
-    // 개방 주소법
-    /*
-    // 충돌 발생 시 빈 버켓에 데이터를 저장하는 방식입니다.
+        data[x].push_back(y);
+        data[y].push_back(x);
 
-    // 빈 버킷을 어떻게 결정할 지에 따라 구현 방식이 달라집니다.
-
-    // 선형 탐사
-    // 충돌 발생 시 앞에서부터 차례대로 빈 버킷을 찾아 값을 저장하는 방식입니다.
-
-    // 이차 탐사
-    // 충돌 발생 시 2^2, 2^3만큼 떨어진 빈 버킷을 찾아 값을 저장하는 방식입니다.
-
-    // 이중 해싱
-    // 해시 값을 한번 더 해시 함수에 넣어 다른 해시값을 도출하는 방식입니다.
-    */
-
+    }
 
 #pragma endregion
 
